69b39920c93ded9a3fa05e0fea3ff14caffa3b31
zhangwei <zhangwei19890518@gmail.com>
2019-02-15 14:06:42 +0800

    init blog

---
+++ .flaskenv
@@ -0,0 +1,3 @@
+FLASK_APP=blog
+FLASK_ENV=development
+BLUELOG_THEMES={'perfect_blue': 'Perfect Blue', 'black_swan': 'Black Swan'}
---
+++ blog/__init__.py
@@ -0,0 +1,123 @@
+#  -*- coding: utf-8 -*-
+
+import os
+
+import click
+from flask import Flask, render_template
+
+from blog.blueprints.admin import admin_bp
+from blog.blueprints.auth import auth_bp
+from blog.blueprints.blog import blog_bp
+from blog.extensions import bootstrap, db, ckeditor, mail, moment
+from blog.models import Admin, Post, Category, Comment, Link
+from blog.settings import config
+
+basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+
+def create_app(config_name=None):
+    if config_name is None:
+        config_name = os.getenv('FLASK_CONFIG', 'development')
+
+    app = Flask('blog')
+    app.config.from_object(config[config_name])
+
+    register_logging(app)
+    register_extensions(app)
+    register_blueprints(app)
+    register_commands(app)
+    register_errors(app)
+    register_shell_context(app)
+    register_template_context(app)
+    return app
+
+
+def register_logging(app):
+    pass
+
+
+def register_extensions(app):
+    bootstrap.init_app(app)
+    db.init_app(app)
+    ckeditor.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+
+
+def register_blueprints(app):
+    app.register_blueprint(blog_bp)
+    app.register_blueprint(admin_bp, url_prefix='/admin')
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+
+def register_shell_context(app):
+    @app.shell_context_processor
+    def make_shell_context():
+        return dict(db=db, Admin=Admin, Post=Post, Category=Category, Comment=Comment)
+
+
+def register_template_context(app):
+    @app.context_processor
+    def make_template_context():
+        admin = Admin.query.first()
+        categories = Category.query.order_by(Category.name).all()
+        links = Link.query.order_by(Link.name).all()
+        return dict(admin=admin, categories=categories, links=links)
+
+
+def register_errors(app):
+    @app.errorhandler(400)
+    def bad_request(e):
+        return render_template('errors/400.html'), 400
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('errors/404.html'), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(e):
+        return render_template('errors/500.html'), 500
+
+
+def register_commands(app):
+    @app.cli.command()
+    @click.option('--drop', is_flag=True, help='Create after drop.')
+    def initdb(drop):
+        """Initialize the database."""
+        if drop:
+            click.confirm('This operation will delete the database, do you want to continue?', abort=True)
+            db.drop_all()
+            click.echo('Drop tables.')
+        db.create_all()
+        click.echo('Initialized database.')
+
+    @app.cli.command()
+    @click.option('--category', default=10, help='Quantity of categories, default is 10.')
+    @click.option('--post', default=50, help='Quantity of posts, default is 50.')
+    @click.option('--comment', default=500, help='Quantity of comments, default is 500.')
+    def forge(category, post, comment):
+        """Generate fake data."""
+        from blog.fakes import fake_admin, fake_categories, fake_posts, fake_comments, fake_links
+
+        db.drop_all()
+        db.create_all()
+
+        click.echo('Generating the administrator...')
+        fake_admin()
+
+        click.echo('Generating %d categories...' % category)
+        fake_categories(category)
+
+        click.echo('Generating %d posts...' % post)
+        fake_posts(post)
+
+        click.echo('Generating %d comments...' % comment)
+        fake_comments(comment)
+
+        click.echo('Generating links...')
+        fake_links()
+
+        click.echo('Done.')
+
+
+
